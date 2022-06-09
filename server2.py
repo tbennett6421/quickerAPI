fcca366e82b255969806f01c52a01836897517cd
Tyler Bennett <tyler.bennett@availity.com>
2022-06-09 13:29:14 -0500

    small changes to boilerplate

--- server2.py
+++ server2.py
@@ -1,11 +1,44 @@
-from hashlib import md5
+__code_project__ = 'threat-toolbox'
+__code_desc__ = "A POC/Learning exercise with FastAPI"
+__code_version__ = 'v0.0.1'
 
+## Standard Libraries
+from pprint import pprint
+
+## Third Party libraries
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
 
-class CustomAPI(FastAPI):
-    def __init__(self, title: str = "CustomAPI") -> None:
-        super().__init__(title=title)
+## Modules
+from classes.funcs import md5,sha1,sha256
+
+tags_metadata = [
+    {
+        "name": "Health",
+        "description": "health checks for this node.",
+    },
+    {
+        "name": "Hash Generation",
+        "description": "endpoints related to generating hashes against their arguments.",
+    },
+    {
+        "name": "default",
+        "description": "uncategorized endpoints.",
+    },
+    # {
+    #     "name": "whois",
+    #     "description": "Whois related endpoints.",
+    # },
+]
+
+
+class Server2(FastAPI):
+    def __init__(self) -> None:
+        super().__init__(
+            title=__code_project__,
+            description=__code_desc__,
+            version=__code_version__,
+            openapi_tags=tags_metadata,
+        )
 
         @self.get("/md5/{param}", tags=['Hash Generation'])
         async def calculate_md5(param: str):
