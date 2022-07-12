146882f9652ecc333e418d25b8dfb42e39f1a17d
Tyler Bennett <tyler.bennett@availity.com>
2022-07-12 14:38:15 -0500

    update unittests; google is no longer number 1 in cisco umbrella

--- tests/routes/alexa_test.py
+++ tests/routes/alexa_test.py
@@ -6,5 +6,6 @@ with TestClient(app) as client:
     def test_fetch_alexa():
         criteria = "google.com"
         response = client.get(f"/alexa/{criteria}")
+        resp_json = response.json()
         assert response.status_code == 200
-        assert response.json() == {'alexa_score': 1}
+        assert(isinstance(resp_json['alexa_score'], int))
--- tests/routes/cisco_test.py
+++ tests/routes/cisco_test.py
@@ -6,5 +6,6 @@ with TestClient(app) as client:
     def test_fetch_cisco():
         criteria = "google.com"
         response = client.get(f"/cisco/{criteria}")
+        resp_json = response.json()
         assert response.status_code == 200
-        assert response.json() == {'cisco_score': 1}
+        assert(isinstance(resp_json['cisco_score'], int))
